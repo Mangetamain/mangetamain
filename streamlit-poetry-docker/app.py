513422135f6c6852da7b94f880d9e8d1abc5aa45
Andranik Hayrapetyan <andranikhayra@gmail.com>
2025-10-02 09:49:35 +0200

    test

--- streamlit-poetry-docker/app.py
+++ streamlit-poetry-docker/app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Docker + Poetry + Streamlit", page_icon="🚀")
 st.title("Mangetamain Docker + Poetry + Streamlit 🚀")
-name = st.text_input("Test input", "Type your name here")
+name = st.text_input("Test input", "Type your name test")
 if name:
     st.success(f"Welcome, {name}!")
